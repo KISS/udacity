b2160412c2bf7fe2477c2586d94dc467a58950c7
KISS <me@luisam.com>
2020-08-17 16:13:43 -0400

    Project 2, P4 - Added test case for empty input array

--- projects/P2/problem_4.py
+++ projects/P2/problem_4.py
@@ -41,3 +41,4 @@ test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0,
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
 test_function([2])
 test_function([1, 0])
+test_function([])
